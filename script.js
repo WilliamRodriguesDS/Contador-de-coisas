0b4136edf8dc2608bbb962c110df9c6db8e0946b
William Rodrigues <william.oliveira@infobrasil.local>
2024-09-06 15:16:29 -0300

    Contador 2.0

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function repetir() {
     let contador = prompt("Escreva qualquer coisa");
     let mensagem = '';
     //criar o estrutura de repetição for
-    for (let i = 1; i <= 10; i = i + 1) {
+    for (let i = 1; i <= 10; i = i += 1) {
         mensagem += "A mensagem "+ contador + " está se repetindo pela " + i + "<br>"
         let output = document.querySelector("#output");
         output.innerHTML = mensagem;
